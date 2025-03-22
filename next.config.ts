3e4f7d623317ac9a1fa3a33278e7ba3d20156601
Reshan Wijerathna <46405486+reshanmadushanka@users.noreply.github.com>
2025-03-22 10:05:23 +0530

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
   eslint: {
     // Disable ESLint during the build process
     ignoreDuringBuilds: true,
