f3a35c24067456b181c53e618908bd2b715dc3fe
Reshan Wijerathna <46405486+reshanmadushanka@users.noreply.github.com>
2025-04-28 07:52:36 +0530

    feat: add Dockerfile and docker-compose.yml; implement DownloadButton component

--- components/DownloadButton.tsx
+++ components/DownloadButton.tsx
@@ -1,26 +1,28 @@
-"use client"; // This directive marks the component as a Client Component
+"use client";
 
 import { FaLocationArrow } from "react-icons/fa6";
 import MagicButton from "./MagicButton";
 
 export default function DownloadButton() {
   function handleClick() {
-    const pdfUrl = "/documents/ReshanWijerathna.pdf";
-    const link = document.createElement("a");
-    link.href = pdfUrl;
-    link.download = "Reshan Wijerathna.pdf";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+    if (typeof window !== "undefined") {
+      const pdfUrl = "/documents/ReshanWijerathna.pdf";
+      const link = document.createElement("a");
+      link.href = pdfUrl;
+      link.download = "Reshan Wijerathna.pdf";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
   }
 
   return (
-    <a onClick={handleClick}>
+    <button onClick={handleClick} type="button">
       <MagicButton
         title="Download CV"
         icon={<FaLocationArrow />}
         position="right"
       />
-    </a>
+    </button>
   );
 }
