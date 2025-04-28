aa150ff6bfef9ccfcbe6500dd45d0d64e08386cf
Reshan Wijerathna <46405486+reshanmadushanka@users.noreply.github.com>
2025-04-28 09:47:21 +0530

    refactor: comment out unused components in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -12,12 +12,12 @@ export default function Home() {
     <main className="relative bg-gray-950 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
-        <Hero />
+        {/* <Hero />
         <Grid />
         <TimelineWork />
         <Experience />
         <Clients />
-        <Footer />
+        <Footer /> */}
       </div>
     </main>
   );
